c6b64cb7d6532644e04bd7a4d428fcda000d24d5
Brandon Green <brandon@brandonleeproductions.com>
2018-09-24 23:54:48 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
 require_relative 'post'
 
-class Author
-  attr_accessor :name, :author, :posts
-
-  @@post_count = 0
-
-  def initialize(name)
-    @name, @posts = name, []
-  end
-
-  def add_post(post)
-    self.posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    self.posts << post
-    post.author = self
-    @@post_count += 1
-=======
 class Author
 attr_accessor :name, :posts, :author
 
@@ -35,16 +13,14 @@ attr_accessor :name, :posts, :author
     @@post_count +=1
     @posts << post
     post.author = self
-
   end
 
   def add_post_by_title(title)
     post= Post.new(title)
-
     self.posts << post
     post.author = self
     @@post_count+=1
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
+
   end
 
   def self.post_count

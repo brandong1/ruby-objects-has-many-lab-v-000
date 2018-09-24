3e76c86e867eeafb7fa0c447ef356b7a1997538f
Brandon Green <brandon@brandonleeproductions.com>
2018-09-24 23:55:57 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,24 +1,19 @@
 class Post
-<<<<<<< HEAD
-  attr_accessor :title, :author
-=======
 
-attr_accessor :title, :author
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
+  attr_accessor :title, :author
 
   def initialize(title)
     @title = title
   end
 
   def author_name
-<<<<<<< HEAD
     self.author.name
   rescue
     nil
   end
 
 end
-=======
+
     if self.author
       self.author.name
     else
@@ -28,4 +23,4 @@ end
 
 
 end 
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
+

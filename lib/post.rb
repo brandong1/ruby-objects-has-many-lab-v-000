62898ce26cf8379dcffc00ae105aa9d1e26ed72c
Brandon Green <brandon@brandonleeproductions.com>
2018-09-24 23:57:50 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,20 +1,20 @@
-class Post
-
-  attr_accessor :title, :author
-
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    self.author.name
-  rescue
-    nil
-  end
-end
-    if self.author
-      self.author.name
-    else
-      nil
-    end
-  end
+# class Post
+# 
+#   attr_accessor :title, :author
+# 
+#   def initialize(title)
+#     @title = title
+#   end
+# 
+#   def author_name
+#     self.author.name
+#   rescue
+#     nil
+#   end
+# end
+#     if self.author
+#       self.author.name
+#     else
+#       nil
+#     end
+#   end

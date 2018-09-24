6027ae4a411368cd3944f30747141540cb6228f6
Brandon Green <brandon@brandonleeproductions.com>
2018-09-24 23:56:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,20 @@
-<<<<<<< HEAD
-=======
 require 'pry'
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
+
 class Song
 
   attr_accessor :name, :artist
 
-<<<<<<< HEAD
-=======
   @@all = []
 
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
   def initialize(name)
     @name = name
   end
 
   def artist_name
-<<<<<<< HEAD
+
     self.artist.name
   rescue
     nil
-=======
     if self.artist
       self.artist.name
     else
@@ -30,7 +24,5 @@ class Song
 
   def self.all
     @@all
->>>>>>> 3ba7d62a8d7fd1df33610109e9e2e0f04ec28f45
   end
-
 end
